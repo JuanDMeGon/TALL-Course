b8b97ca5898af1a3e2ade8fd09546936ec6d616d
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-12-16 18:15:27 -0500

    Meet Tailwind and Use It to Start with  the Landing Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,25 @@
 <x-guest-layout>
-    <x-application-logo></x-application-logo>
+    <div class="flex flex-col bg-indigo-900 w-full h-screen">
+        <nav class="flex pt-5 justify-between container mx-auto text-indigo-200">
+            <a class="text-4xl font-bold" href="/">
+                <x-application-logo class="w-16 h-16 fill-current"></x-application-logo>
+            </a>
+
+            <div class="flex justify-end">
+                @auth
+                    <a href="{{ route('dashboard') }}">
+                        Dashboard
+                    </a>
+                @else
+                    <a href="{{ route('login') }}">
+                        Login
+                    </a>
+                @endauth
+            </div>
+        </nav>
+    </div>
+
+    <div class="flex flex-col bg-pink-500 w-full h-screen">
+
+    </div>
 </x-guest-layout>
