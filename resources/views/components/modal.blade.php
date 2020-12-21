a8333a6e65d37b0a1188c8830b69a35eff5c3ec7
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-12-21 16:33:31 -0500

    Modularize the Modals Using the Blade Components of Laravel

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,12 @@
+@props(['trigger'])
+
+<div
+    class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full"
+    x-show="{{ $trigger }}"
+    x-on:click.self="{{ $trigger }} = false"
+    x-on:keydown.escape.window="{{ $trigger }} = false"
+>
+    <div {{ $attributes->merge(['class' => 'm-auto bg-gray-200 shadow-2xl rounded-xl p-8']) }}>
+        {{ $slot }}
+    </div>
+</div>
--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -1,8 +1,8 @@
 <div
     class="flex flex-col bg-indigo-900 w-full h-screen"
     x-data="{
-        showSubscribe: true,
-        showSuccess: true,
+        showSubscribe: false,
+        showSuccess: false,
     }"
 >
     <nav class="flex pt-5 justify-between container mx-auto text-indigo-200">
@@ -42,52 +42,39 @@
         </div>
     </div>
 
-    <div
-        class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full"
-        x-show="showSubscribe"
-        x-on:click.self="showSubscribe = false"
-        x-on:keydown.escape.window="showSubscribe = false"
-    >
-        <div class="m-auto bg-pink-500 shadow-2xl rounded-xl p-8">
-            <p class="text-white text-5xl font-extrabold text-center">
-                Let's do it!
-            </p>
-            <form
-                class="flex flex-col items-center p-24"
-                wire:submit.prevent="subscribe"
-            >
-                <x-input
-                    class="px-5 py-3 w-80 border border-blue-400"
-                    type="email"
-                    name="email"
-                    placeholder="Email address"
-                    wire:model="email"
-                ></x-input>
-                <span class="text-gray-100 text-xs">
-                    We will send you a confirmation email.
-                </span>
-                <x-button class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center">
-                    Get In
-                </x-button>
-            </form>
-        </div>
-    </div>
-    <div
-        class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full"
-        x-show="showSuccess"
-        x-on:click.self="showSuccess = false"
-        x-on:keydown.escape.window="showSuccess = false"
-    >
-        <div class="m-auto bg-green-500 shadow-2xl rounded-xl p-8">
-            <p class="animate-pulse text-white text-9xl font-extrabold text-center">
-                &check;
-            </p>
-            <p class="text-white text-5xl font-extrabold text-center mt-16">
-                Great!
-            </p>
-            <p class="text-white text-3xl text-center">
-                See you in your inbox.
-            </p>
-        </div>
-    </div>
+    <x-modal class="bg-pink-500" trigger="showSubscribe">
+        <p class="text-white text-5xl font-extrabold text-center">
+            Let's do it!
+        </p>
+        <form
+            class="flex flex-col items-center p-24"
+            wire:submit.prevent="subscribe"
+        >
+            <x-input
+                class="px-5 py-3 w-80 border border-blue-400"
+                type="email"
+                name="email"
+                placeholder="Email address"
+                wire:model="email"
+            ></x-input>
+            <span class="text-gray-100 text-xs">
+                We will send you a confirmation email.
+            </span>
+            <x-button class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center">
+                Get In
+            </x-button>
+        </form>
+    </x-modal>
+
+    <x-modal class="bg-green-500" trigger="showSuccess">
+        <p class="animate-pulse text-white text-9xl font-extrabold text-center">
+            &check;
+        </p>
+        <p class="text-white text-5xl font-extrabold text-center mt-16">
+            Great!
+        </p>
+        <p class="text-white text-3xl text-center">
+            See you in your inbox.
+        </p>
+    </x-modal>
 </div>
